0c14fcdee0f086e40990bfa6a8e565d3e1039dd0
VitorHugo05 <vitorhugo.pozzi@gmail.com>
2025-01-10 15:53:47 -0300

    first commit

---
+++ src/test/java/com/vitordev/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitordev.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
