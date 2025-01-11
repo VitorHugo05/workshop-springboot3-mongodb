76d475e48f53da2020392023f33b9309c12dc984
VitorHugo05 <vitorhugo.pozzi@gmail.com>
2025-01-11 15:20:05 -0300

    Consulta simples com @Query

--- src/main/java/com/vitordev/workshopmongodb/services/PostService.java
+++ src/main/java/com/vitordev/workshopmongodb/services/PostService.java
@@ -25,6 +25,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String title) {
-        return postRepository.findByTitleContainingIgnoreCase(title);
+        return postRepository.searchTitle(title);
     }
 }
