f58df475bf1cccb3f2e5b3ecf35f75e4a3b5ef71
VitorHugo05 <vitorhugo.pozzi@gmail.com>
2025-01-11 15:12:17 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/vitordev/workshopmongodb/resources/util/URL.java
@@ -0,0 +1,14 @@
+package com.vitordev.workshopmongodb.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+    public static String decodeParam(String str) {
+        try {
+            return URLDecoder.decode(str, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
+    }
+}
